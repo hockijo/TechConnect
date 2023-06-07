107420f107b04d9cca14d8c77b2ae5d6653ccafe
Joseph Hocking <22876965@student.uwa.edu.au>
2023-06-07 16:46:23 +0800

    Added support for the Agilen33250A

--- signal_generators.py
+++ signal_generators.py
@@ -2,10 +2,14 @@ import numpy as np
 import pyvisa
 import os
 import time
-from VISA_instrument import VISAInstrument
+from VISA_instrument import VISAInstrument, PrologixInstrument
 
 
 class DG1000(VISAInstrument):
+    def __init__(self):
+        super().__init__()
+        self.query_delay = 1
+
     def query_apply(self, channel):
         if channel == 1:
             query = self.query_SCPI(f"APPLY?")
@@ -55,20 +59,62 @@ class DG1000(VISAInstrument):
     
 
 
-""" class Agilent33250A(VISASignalGenerator):
+class Agilent33250A(PrologixInstrument):
+    def __init__(self):
+        super().__init__()
+        self.query_delay = 0.1
+        self.gpib_address = 10
+
+    def query_apply(self, channel):
+        if channel == 1:
+            query = self.query_SCPI(f"APPLY?")
+        elif channel == 2:
+            query = self.query_SCPI(f"APPLY:CH{channel}?")
+        print(f"Signal Generator setup {query}")
+        return query
+    
+    def turnOn(self, channel):
+        if channel == 1:
+            lines = ("OUTPUT ON",)
+        if channel == 2:
+            lines = ("OUTPUT:CH2 ON",)
+
+        self.write_lines(lines)
+
+    def turnOff(self, channel):
+        if channel == 1:
+            lines = ("OUTPUT OFF",)
+        if channel == 2:
+            lines = ("OUTPUT:CH2 OFF",)
+
+        self.write_lines(lines)
+
+    def setupSine(self, frequency, amplitude_pp, offset=0, phase=0, channel=1):
+        self.setupFunc("SIN", frequency, amplitude_pp, offset, phase, channel=channel)
+
+    def setupFunc(self, func: str, frequency, amplitude_pp, offset=0, phase=0, channel=1):
+        if channel == 1:
+            lines = (f"FUNC {func.upper()}",
+                    f"FREQ {float(frequency)}",
+                    f"VOLT {float(amplitude_pp)}",
+                    f"VOLT:OFFSET {float(offset)}",
+                )
+            
+        elif channel == 2:
+            lines = (f"FUNC:CH{channel} {func.upper()}",
+                    f"FREQ:CH{channel} {float(frequency)}",
+                    f"VOLT:CH{channel} {float(amplitude_pp)}",
+                    f"VOLT:CH{channel}:OFFSET {float(offset)}",
+                )
+
+        self.write_lines(lines)
+        _ = self.query_apply(channel)
+        
+    
     
 
-
+"""
 class DG4000(VISASignalGenerator):
     def query_apply(self):
         return self.query_SCPI("APPLY?")
- """
-
-dg1022u_address = r"USB0::0x0400::0x09C4::DG1G170900658::INSTR"
-dg1022u = DG1000()
-dg1022u.connect(dg1022u_address)
-
-rm = pyvisa.ResourceManager()
-my_instr = rm.open_resource(dg1022u_address)
-
-print()
+"""
