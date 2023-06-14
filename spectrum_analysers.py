a55fbfda51805bc4820889c592f8d840f758983b
Joseph Hocking <22876965@student.uwa.edu.au>
2023-06-14 15:38:21 +0800

    Added spectrum analysers script

---
+++ spectrum_analysers.py
@@ -0,0 +1,5 @@
+import numpy as np 
+from VISA_instrument import VISAInstrument, PrologixInstrument
+
+class Agilent89410A(PrologixInstrument):
+    raise NotImplementedError("This instrument is not implemented")
