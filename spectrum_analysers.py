0081d481dc5795c12cc092bb9a9ee6ae2583df35
Joseph Hocking <22876965@student.uwa.edu.au>
2023-07-11 11:54:38 +0800

    Began implementing Agilent89410A

--- spectrum_analysers.py
+++ spectrum_analysers.py
@@ -2,4 +2,19 @@ import numpy as np
 from VISA_instrument import VISAInstrument, PrologixInstrument
 
 class Agilent89410A(PrologixInstrument):
-    raise NotImplementedError("This instrument is not implemented")
+    def __init__(self):
+        super().__init__()
+        self.query_delay = 0.1
+        self.instrument_type = "Agilent89410A Spectrum Analyser"
+        self.initialise_device()
+
+    def calibrate(self):
+        lines = (
+            "CALIBRATION:AUTO ONCE",
+            "CALIBRATION:ZERO:AUTO ONCE",
+            "CALIBRATION:AUTO ON",
+            "CALIBRATION:ZERO:AUTO ON"
+        )
+        self.write_lines(lines)
+
+
