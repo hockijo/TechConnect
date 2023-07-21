f4037a09079382b10b38f4f496c753737405ffc8
Joseph Hocking <22876965@student.uwa.edu.au>
2023-07-21 16:37:21 +0800

    Added docstrings

--- VISA_instrument.py
+++ VISA_instrument.py
@@ -10,14 +10,40 @@ class VISAInstrument():
         self.rm = pyvisa.ResourceManager()
 
     def connect(self, address):
+        """
+        Connects to an instrument at the specified address.
+
+        Parameters:
+            address (str): The address of the instrument.
+
+        Returns:
+            None
+        """
         self.instrument = self.rm.open_resource(address, open_timeout=2, read_termination='\n')
         self.instrument.query_delay = self.query_delay
-        print(f"Successfully connected to signal generator with address {address}")
+        print(f"Successfully connected to instrument with address {address}")
 
     def initialise_device(self):
+        """
+        Initializes the device.
+
+        This function is responsible for initializing the device. It clears any existing data on the instrument.
+
+        Returns:
+            None
+        """
         self.instrument.clear()
 
     def list_connections(self, verbose=True):
+        """
+        List connections and print the resource information if verbose is True. Otherwise, print only the resource names.
+
+        Parameters:
+            verbose (bool, optional): Whether to print resource information. Defaults to True.
+
+        Returns:
+            list: A list of resource names.
+        """
         if verbose:
             print(self.rm.list_resources_info())
         else:
@@ -25,25 +51,67 @@ class VISAInstrument():
         return self.rm.list_resources()
 
     def write_SCPI(self, command: str):
+        """
+        Writes an SCPI command to the instrument.
+
+        Parameters:
+            command (str): The SCPI command to be written.
+
+        Returns:
+            None
+        """
         print(command)
         self.instrument.write(command)
         #error = self.check_errors()
 
     def query_SCPI(self, query: str):
+        """
+        Execute a SCPI query command and return the response.
+
+        Parameters:
+            query (str): The SCPI query command to send to the instrument.
+
+        Returns:
+            str: The response received from the instrument.
+
+        """
         query = self.instrument.query(query)
         #error = self.check_errors()
         return query
 
     def write_lines(self, lines):
+        """
+        Writes a list of lines to the device.
+
+        Parameters:
+            lines (list): The list of lines to write.
+
+        Returns:
+            None
+        """
         for line in lines:
             self.write_SCPI(line)
             time.sleep(0.1)
 
     def close_device(self):
+        """
+        Closes the device.
+
+        This function clears the instrument and then closes the connection to the device.
+
+        Returns:
+            None
+        """
         self.instrument.clear()
         self.instrument.close()
 
     def check_errors(self):
+        """
+        Check for errors by querying the SCPI and handling the returned error.
+
+        Returns:
+            str: The error message returned by the SCPI query.
+        """
         #TODO: implement return error handling
         error = self.query_SCPI("SYSTEM:ERROR?")
         if error != "+0, No error":
@@ -53,12 +121,32 @@ class VISAInstrument():
 
 class PrologixInstrument(VISAInstrument):
     def connect(self, address):
+        """
+        Connects to an instrument at the specified address.
+
+        Parameters:
+            address (str): The address of the instrument to connect to.
+
+        Returns:
+            None
+        """
+        
         self.instrument = PrologixAdapter(address, self.gpib_address, timeout=1000) # open_timeout=2, read_termination='\n', resource_pyclass=Prologix
         self.instrument.query_delay = self.query_delay
         #self.instrument.configure(vna_gpib_address=self.gpib_address)
-        print(f"Successfully connected to signal generator with address {address}")
+        print(f"Successfully connected to instrument with address {address}")
 
     def query_SCPI(self, query: str):
+        """
+        Send a query to the SCPI instrument and return the response.
+
+        Parameters:
+            query (str): The query to send to the instrument.
+
+        Returns:
+            str: The response from the instrument.
+
+        """
         self.instrument.write(query)
         time.sleep(self.query_delay)
         query = self.instrument.read().strip('\n')
