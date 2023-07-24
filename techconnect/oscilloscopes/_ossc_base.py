23bcd02a8524a5643d7ec5261a92a2938c3b90f4
Joseph Hocking <22876965@student.uwa.edu.au>
2023-07-24 11:40:34 +0800

    added docstrings

--- techconnect/oscilloscopes/_ossc_base.py
+++ techconnect/oscilloscopes/_ossc_base.py
@@ -1,3 +1,8 @@
+"""
+The base class for oscilloscopes, containing methods for parsing and saving data. This is kept private
+
+Pieces salvaged from InfiniiVision_SegmentedMemory_Waveform.py
+"""
 import numpy as np 
 import pickle as pkl
 import os
@@ -7,11 +12,10 @@ import techconnect.tools.file_handling as file_handling
 
 __all__ = []
 
-################################################################
-#Pieces salvaged from InfiniiVision_SegmentedMemory_Waveform.py#
-################################################################
-
-class oscilloscope(VISAInstrument):
+class Oscilloscope(VISAInstrument):
+    """
+    Base Class representing an oscilloscope.
+    """
     def __init__(self):
         super().__init__()
         self.query_delay = 0.1
@@ -53,6 +57,26 @@ class oscilloscope(VISAInstrument):
         raise NotImplementedError()
 
     def generate_x_data(self, channel_info):
+        """
+        Generate the x data based on the channel information.
+
+        Parameters:
+        -----------
+        channel_info: dict
+            A dictionary containing the channel information.
+            It should have the following keys:
+            - 'points': The number of points.
+            - 'xreference': The x reference value.
+            - 'xincrement': The x increment value.
+            - 'xorigin': The x origin value.
+            - 'type': The type of channel.
+
+        Returns:
+        --------
+        ndarray
+            The generated x data.
+
+        """
         points = channel_info['points']
         x_data = ((np.linspace(0, points - 1, points) - channel_info['xreference'])*channel_info['xincrement']) + channel_info['xorigin']
         if channel_info['type'] == 1:
@@ -60,15 +84,70 @@ class oscilloscope(VISAInstrument):
         return x_data
     
     def stitch_multiseg_x_data(self, x_data, time_tags):
+        """
+        Concatenates the given `x_data` with each element in `time_tags`, constructing a stitched single array for the x data.
+
+        Parameters:
+        -----------
+        x_data: numpy.ndarray
+            The input data to be concatenated.
+        time_tags: iterable
+            A list of time tags to concatenate with `x_data`.
+
+        Returns:
+        --------
+        numpy.ndarray
+            The concatenated array.
+        """
         return np.concatenate([x_data + time_tag for time_tag in time_tags])
     
     def scale_y_data(self, y_data, channel_info):
+        """
+        Scales the given y data based on the channel information.
+
+        Parameters
+        ----------
+        y_data : array-like
+            The y data to be scaled.
+        channel_info : dict
+            A dictionary containing channel information, including 'yreference', 'yincrement', and 'yorigin'.
+
+        Returns
+        -------
+        array-like
+            The scaled y data.
+        """
         return ((y_data - channel_info['yreference'])*channel_info['yincrement']) + channel_info['yorigin']
     
     def collect_single_segment_data(self, segment_index, channel, channel_info):
         raise NotImplementedError()
     
     def collect_multi_segment_data(self, number_of_segments, channel, stitched: bool):
+        """
+        Collects multi-segment data for a given number of segments, channel, and stitched flag.
+
+        Parameters
+        ----------
+        number_of_segments : int
+            The number of segments to collect data from.
+        channel : str
+            The channel to collect data from.
+        stitched : bool
+            Flag indicating whether the data should be stitched.
+
+        Returns
+        -------
+        tuple
+            A tuple containing the following:
+            x_data : ndarray
+                The x-axis data.
+            y_data : ndarray
+                The y-axis data.
+            time_tags : ndarray
+                The time tags.
+            channel_info : str
+                The channel information.
+        """
         self.data_export_setup(channel)
         channel_info = self.retrieve_channel_info(channel)
 
@@ -93,6 +172,22 @@ class oscilloscope(VISAInstrument):
         return x_data, y_data, time_tags, channel_info
     
     def parse_and_save(self, save_directory, save_dict, acquisition_info):
+        """
+        Parses the data and saves it to a specified directory.
+
+        Parameters
+        ----------
+        save_directory : str
+            A string representing the directory where the data will be saved.
+        save_dict : dict
+            A dictionary containing the data to be saved.
+        acquisition_info : dict, optional
+            An optional dictionary containing additional acquisition information.
+
+        Returns
+        -------
+        None
+        """
         filename = f'{time.strftime(r"%d-%m-%Y-%H-%M-%S", time.localtime())}_data_collection'
 
         metadata = {
@@ -111,6 +206,27 @@ class oscilloscope(VISAInstrument):
         file_handling.save_to_pkl(save_dict, metadata=metadata, directory=save_directory, filename=filename)
 
     def stitched_data_acquisition(self, acquisition_time, channels, segment_number=1000, save_directory=None, acquisition_type='HRESOLUTION'):
+        """
+        Perform stitched data acquisition.
+
+        Parameters
+        ----------
+        acquisition_time : float
+            The total acquisition time in seconds.
+        channels : list
+            A list of channels to acquire.
+        segment_number : int, optional
+            The number of segments to divide the acquisition into. Defaults to 1000.
+        save_directory : str, optional
+            The directory to save the acquired data. `None` will not save, otherwise will create and save to given directory. Defaults to None.
+        acquisition_type : str, optional
+            The acquisition type. Defaults to 'HRESOLUTION'.
+
+        Returns
+        -------
+        tuple
+            A tuple containing the acquired x data, y data, time tags, and channel info.
+        """
         self.auto_setup()
         time_window = acquisition_time/segment_number
         self.setup_time_window(time_window)
@@ -146,6 +262,27 @@ class oscilloscope(VISAInstrument):
         return x_data, y_data, time_tags, channel_info
     
     def unstitched_data_acquisition(self, time_window, segment_number, channels, save_directory=None, acquisition_type='HRESOLUTION'):
+        """
+        Perform unstitched data acquisition for a given time window, segment number, and channels.
+
+        Parameters
+        ----------
+        time_window : float
+            The time window for each segment in seconds.
+        segment_number : int
+            The number of segments to acquire.
+        channels : list
+            A list of channels to acquire data from.
+        save_directory : str, optional
+            The directory to save the acquired data. Default is None.
+        acquisition_type : str, optional
+            The type of acquisition. Default is 'HRESOLUTION'.
+
+        Returns
+        -------
+        tuple
+            A tuple containing the acquired x_data, y_data, time_tags, and channel_info.
+        """
         self.auto_setup()
         self.setup_time_window(time_window)
         self.setup_triggering()
--- techconnect/oscilloscopes/keysight.py
+++ techconnect/oscilloscopes/keysight.py
@@ -1,23 +1,93 @@
-import numpy as np 
-import pickle as pkl
+"""
+This module contqains the instrument classesn for keysight oscilloscopes. It uses the methods created in the base class for the data parsing and saving,
+but the communication is handled by the methods defined here.
+
+Pieces salvaged from InfiniiVision_SegmentedMemory_Waveform.py
+"""
 import os
 import time
+
+import numpy as np 
+import pickle as pkl
+
 from techconnect.base._instrument import VISAInstrument
 import techconnect.tools.file_handling as file_handling
-from techconnect.oscilloscopes._ossc_base import oscilloscope 
+from techconnect.oscilloscopes._ossc_base import Oscilloscope 
 
 __all__ = ['Keysight3000T']
 
-################################################################
-#Pieces salvaged from InfiniiVision_SegmentedMemory_Waveform.py#
-################################################################
+class Keysight3000T(Oscilloscope):
+    """
+    Class representing the Keysight 3000T series oscilloscope.
 
-class Keysight3000T(oscilloscope):
+    Attributes
+    ----------
+    query_delay : float
+        The delay between query and read in seconds.
+
+    Methods
+    -------
+    __init__()
+        Initializes the Keysight3000T object.
+    segmented_initialization(channel)
+        Perform segmented initialization for a specific channel.
+    calculate_segment_number(acquistion_time)
+        Calculate the number of segments based on the acquisition time.
+    setup_time_window(time_window)
+        Set up the time window for each segment.
+    setup_triggering()
+        Set up the triggering mode for the oscilloscope.
+    segmented_acquistion_setup(segment_count, acquistion_type='NORMAL', sample_rate='AUTO')
+        Set up the segmented acquisition mode.
+    manual_channel_setup(channel, vertical_scale, vertical_range, vertical_offset, bw_limit, coupling)
+        Perform manual setup for a specific channel.
+    auto_setup()
+        Perform automatic setup for the oscilloscope.
+    single_acquisition()
+        Perform a single acquisition.
+    digitize_acquisition(channels)
+        Digitize the acquisition for the specified channels.
+    retrieve_channel_info(channel)
+        Retrieve information about a specific channel.
+    data_export_setup(channel)
+        Set up data export for a specific channel.
+    get_number_of_points(channel)
+        Get the number of points for a specific channel.
+    generate_x_data(channel_info)
+        Generate the x-axis data based on the channel information.
+    stitch_multiseg_x_data(x_data, time_tags)
+        Stitch together the x-axis data for multi-segment acquisition.
+    scale_y_data(y_data, channel_info)
+        Scale the y-axis data based on the channel information.
+    collect_single_segment_data(segment_index, channel, channel_info)
+        Collect data for a single segment and channel.
+    collect_multi_segment_data(number_of_segments, channel, stitched)
+        Collect data for multiple segments and a specific channel.
+    parse_and_save(save_directory, save_dict, acquisition_info)
+        Parse and save the acquired data.
+    stitched_data_acquisition(acquisition_time, channels, segment_number=1000, save_directory=None, acquisition_type='HRESOLUTION')
+        Perform stitched data acquisition.
+    unstitched_data_acquisition(time_window, segment_number, channels, save_directory=None, acquisition_type='HRESOLUTION')
+        Perform unstitched data acquisition.
+    """
+
     def __init__(self):
         super().__init__()
         self.query_delay = 0.1
 
     def segmented_initialization(self, channel):
+        """
+        Initializes the given channel by turning on the display, enabling automatic sample rate selection, and enabling automatic points selection.
+
+        Parameters
+        ----------
+        channel : int
+            The channel number to be initialized.
+
+        Returns
+        -------
+        None
+        """
         lines = (
             f":CHAN{channel}:DISPLAY ON",
             f":ACQUIRE:CHAN {channel}:SRATE:AUTO ON",
@@ -26,17 +96,46 @@ class Keysight3000T(oscilloscope):
 
         self.write_lines(lines)
 
-    def calculate_segment_number(self, acquistion_time):
+    def calculate_segment_number(self, acquisition_time):
+        """
+        Calculate the segment number based on the acquisition time.
+
+        Parameters
+        ----------
+        acquisition_time : float
+            The acquisition time in seconds.
+
+        Returns
+        -------
+        segment_number : int
+            The calculated segment number.
+        """
         sample_rate = int(float(self.query_SCPI(f":ACQUIRE:SRATE?")))
         points_per_segment = int(float(self.query_SCPI(f":ACQUIRE:POINTS?")))
         
-        segment_number = int(np.ceil((acquistion_time*sample_rate)/points_per_segment))
+        segment_number = int(np.ceil((acquisition_time*sample_rate)/points_per_segment))
         return segment_number
     
     def setup_time_window(self, time_window):
+        """
+        Sets up the time window for the instrument.
+
+        Parameters
+        ----------
+        time_window : float
+            The time window value to be set.
+
+        Returns
+        -------
+        None
+            This function does not return anything.
+        """
         self.write_SCPI(f":TIMEBASE:SCALE {time_window/10}")
 
     def setup_triggering(self):
+        """
+        Sets up triggering for the device. It will be edge triggered externally, and on a positive slope
+        """
         lines = (":TRIGGER:MODE EDGE",
                  ":TRIGGER:EDGE:SOURCE EXT",
                  ":TRIGGER:EDGE:SLOPE POSITIVE")
@@ -46,6 +145,22 @@ class Keysight3000T(oscilloscope):
                                     acquistion_type='NORMAL',
                                     sample_rate='AUTO'
                                     ):
+        """
+        Set up the segmented acquisition mode for the oscilloscope.
+
+        Parameters
+        ----------
+        segment_count : int
+            The number of segments to acquire.
+        acquistion_type : str, optional
+            The type of acquisition. Defaults to 'NORMAL'.
+        sample_rate : str, optional
+            The sample rate. Defaults to 'AUTO'.
+
+        Returns
+        -------
+        None
+        """
 
         lines = (
             f":ACQUIRE:MODE SEGMENTED",
@@ -62,7 +177,29 @@ class Keysight3000T(oscilloscope):
                       bw_limit,
                       coupling,
                       ):
-        
+        """
+        Sets up the manual configuration for a specific channel.
+
+        Parameters
+        ----------
+        channel : int
+            The channel number.
+        vertical_scale : 
+            The vertical scale.
+        vertical_range : 
+            The vertical range.
+        vertical_offset : 
+            The vertical offset.
+        bw_limit : 
+            The bandwidth limit.
+        coupling : 
+            The coupling type.
+
+        Returns
+        -------
+        None
+            This function does not return anything.
+        """
         lines = (
             f":CHAN{channel}:DISPLAY ON",
             f":CHAN{channel}:BWLIMIT{bw_limit}",
@@ -75,12 +212,31 @@ class Keysight3000T(oscilloscope):
         self.write_lines(lines)
 
     def auto_setup(self):
+        """
+        Automatically sets up the instrument by sending the SCPI command ":AUTOSCALE".
+        """
         self.write_SCPI(f":AUTOSCALE")
 
     def single_acquisition(self):
+        """
+        Sends a command to the SCPI instrument to perform a single acquisition.
+        """
         self.write_SCPI(f":SINGLE")
 
     def digitize_acquisition(self, channels):
+        """
+        Digitize the acquisition for the given channels.
+
+        Parameters
+        ----------
+        channels : list of int
+            A list of channel numbers to digitize.
+
+        Returns
+        -------
+        None
+            This function does not return anything.
+        """
         line = ":DIGITIZE"
         if len(channels)>1:
             for channel in channels:
@@ -88,22 +244,20 @@ class Keysight3000T(oscilloscope):
 
         self.write_SCPI(line)
 
-    def retrieve_channel_info(self, channel):
-        channel_info = self.query_SCPI(f":WAVEFORM:SOURCE CHAN{channel};:WAVEFORM:PREAMBLE?").split(',')
-        return {
-            'format': int(channel_info[0]),
-            'type': int(channel_info[1]),
-            'points': int(float(channel_info[2])),
-            'count': int(channel_info[3]),
-            'xincrement': float(channel_info[4]),
-            'xorigin': float(channel_info[5]),
-            'xreference': float(channel_info[6]),
-            'yincrement': float(channel_info[7]),
-            'yorigin': float(channel_info[8]),
-            'yreference': float(channel_info[9])
-        }
-    
     def data_export_setup(self, channel):
+        """
+        Set up the data export for a specific channel.
+
+        Parameters
+        ----------
+        channel : int
+            The channel number.
+
+        Returns
+        -------
+        None
+            This function does not return anything.
+        """
         lines = (
             f":WAVEFORM:SOURCE CHAN{channel}",
             f":WAVEFORM:FORMAT WORD",
@@ -116,22 +270,39 @@ class Keysight3000T(oscilloscope):
         self.write_lines(lines)
 
     def get_number_of_points(self, channel):
-        return int(self.write_SCPI(f"WAVEFORM:SOURCE CHAN{channel};:WAVEFORM:POINTS?"))
+        """
+        Get the number of points in a waveform for a given channel.
 
-    def generate_x_data(self, channel_info):
-        points = channel_info['points']
-        x_data = ((np.linspace(0, points - 1, points) - channel_info['xreference'])*channel_info['xincrement']) + channel_info['xorigin']
-        if channel_info['type'] == 1:
-            x_data = np.repeat(x_data, 2)
-        return x_data
-    
-    def stitch_multiseg_x_data(self, x_data, time_tags):
-        return np.concatenate([x_data + time_tag for time_tag in time_tags])
-    
-    def scale_y_data(self, y_data, channel_info):
-        return ((y_data - channel_info['yreference'])*channel_info['yincrement']) + channel_info['yorigin']
+        Parameters
+        ----------
+        channel : str
+            The channel number.
+
+        Returns
+        -------
+        int
+            The number of points in the waveform.
+        """
+        return int(self.write_SCPI(f"WAVEFORM:SOURCE CHAN{channel};:WAVEFORM:POINTS?"))
     
     def collect_single_segment_data(self, segment_index, channel, channel_info):
+        """
+        Collects data for a single segment in the waveform.
+
+        Parameters
+        ----------
+        segment_index : int
+            The index of the segment to collect data from.
+        channel : str
+            The channel to collect data from.
+        channel_info : dict
+            A dictionary containing information about the channel.
+
+        Returns
+        -------
+        tuple
+            A tuple containing the segment time tag (float) and the collected data (ndarray).
+        """
         self.write_SCPI(f":ACQUIRE:SEGMENTED:INDEX {segment_index}")
         segment_time_tag = float(self.query_SCPI(f":WAVEFORM:SEGMENTED:TTAG?"))
 
@@ -139,115 +310,4 @@ class Keysight3000T(oscilloscope):
         y_data = self.scale_y_data(y_data, channel_info)
 
         return segment_time_tag, y_data
-    
-    def collect_multi_segment_data(self, number_of_segments, channel, stitched: bool):
-        self.data_export_setup(channel)
-        channel_info = self.retrieve_channel_info(channel)
-
-        segment_x_data = self.generate_x_data(channel_info)
-
-        time_tags = []
-        y_data = []
-        for segment in range(1, number_of_segments+1):
-            segment_time_tag, segment_y_data = self.collect_single_segment_data(segment, channel, channel_info)
-            time_tags.append(segment_time_tag)
-            y_data.append(segment_y_data)
-
-        time_tags = np.asarray(time_tags)
-        y_data = np.asarray(y_data)
-
-        if stitched:
-            x_data = self.stitch_multiseg_x_data(segment_x_data, time_tags)
-            y_data = np.reshape(y_data, x_data.shape)
-        else:
-            x_data = segment_x_data
-
-        return x_data, y_data, time_tags, channel_info
-    
-    def parse_and_save(self, save_directory, save_dict, acquisition_info):
-        filename = f'{time.strftime(r"%d-%m-%Y-%H-%M-%S", time.localtime())}_data_collection'
-
-        metadata = {
-                'resource_info': self.instrument.resource_info._asdict().copy(),
-                'collection_time_UTC': time.time()
-            }
-        metadata['resource_info'].update({'manufactuer_id': self.instrument.manufacturer_id,
-                                            'manufactuer_name': self.instrument.manufacturer_name,
-                                            'model_code': self.instrument.model_code,
-                                            'model_name': self.instrument.model_name,
-                                            'serial_number': self.instrument.serial_number,
-                                            'idn': self.query_SCPI(u"*IDN?")})
-        if acquisition_info is not None:
-            metadata.update({'acquisition_info': acquisition_info})
-        
-        file_handling.save_to_pkl(save_dict, metadata=metadata, directory=save_directory, filename=filename)
-
-    def stitched_data_acquisition(self, acquisition_time, channels, segment_number=1000, save_directory=None, acquisition_type='HRESOLUTION'):
-        self.auto_setup()
-        time_window = acquisition_time/segment_number
-        self.setup_time_window(time_window)
-        self.setup_triggering()
-        for channel in channels:
-            self.segmented_initialization(channel)
-        self.segmented_acquistion_setup(segment_number, acquistion_type=acquisition_type)
-
-        self.digitize_acquisition(channels)
-        time.sleep(1.5*acquisition_time+2)
-
-        x_data = {}
-        y_data = {}
-        time_tags = {}
-        channel_info = {}
-        for channel in channels:
-            channel_x_data, channel_y_data, channel_time_tags, channel_channel_info = self.collect_multi_segment_data(segment_number, channel, stitched=True)
-            x_data.update({channel: channel_x_data})
-            y_data.update({channel: channel_y_data})
-            time_tags.update({channel: channel_time_tags})
-            channel_info.update({channel: channel_channel_info})
-
-        if save_directory is not None:
-            save_dict = {
-                            'x_data': x_data,
-                            'y_data': y_data,
-                            'time_tags': time_tags,
-                            'channel_info': channel_info
-                        }
-            acquisition_info = {'type': acquisition_type, 'acq_time': acquisition_time, "segment_number": segment_number, 'time_window': time_window, 'stitched': True}
-            self.parse_and_save(save_directory, save_dict, acquisition_info)
-
-        return x_data, y_data, time_tags, channel_info
-    
-    def unstitched_data_acquisition(self, time_window, segment_number, channels, save_directory=None, acquisition_type='HRESOLUTION'):
-        self.auto_setup()
-        self.setup_time_window(time_window)
-        self.setup_triggering()
-        for channel in channels:
-            self.segmented_initialization(channel)
-        self.segmented_acquistion_setup(segment_number, acquistion_type=acquisition_type)
-
-        self.digitize_acquisition(channels)
-        time.sleep(3*(time_window*segment_number)+0.1*segment_number)
-
-        x_data = {}
-        y_data = {}
-        time_tags = {}
-        channel_info = {}
-        for channel in channels:
-            channel_x_data, channel_y_data, channel_time_tags, channel_channel_info = self.collect_multi_segment_data(segment_number, channel, stitched=False)
-            x_data.update({channel: channel_x_data})
-            y_data.update({channel: channel_y_data})
-            time_tags.update({channel: channel_time_tags})
-            channel_info.update({channel: channel_channel_info})
-
-        if save_directory is not None:
-            save_dict = {
-                            'x_data': x_data,
-                            'y_data': y_data,
-                            'time_tags': time_tags,
-                            'channel_info': channel_info
-                        }
-            acquisition_info = {'type': acquisition_type, 'acq_time': time_window*segment_number, "segment_number": segment_number, 'time_window': time_window, 'stitched': False}
-            self.parse_and_save(save_directory, save_dict, acquisition_info)
-
-        return x_data, y_data, time_tags, channel_info
 
