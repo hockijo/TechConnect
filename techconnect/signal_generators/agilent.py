f966f35d3aad5048761551ff838110f77fb6af14
Joseph Hocking <22876965@student.uwa.edu.au>
2023-07-24 16:31:48 +0800

    added docstrings

--- techconnect/signal_generators/agilent.py
+++ techconnect/signal_generators/agilent.py
@@ -3,26 +3,125 @@ from base._instrument import VISAInstrument, PrologixInstrument
 
 
 class Agilent33250A(PrologixInstrument):
+    """
+    Class representing the Agilent 33250A signal generator.
+
+    Parameters
+    ----------
+    gpib_address : int
+        The GPIB address of the signal generator.
+
+    Attributes
+    ----------
+    query_delay : float
+        The delay time (in seconds) for SCPI query commands.
+    gpib_address : int
+        The GPIB address of the signal generator.
+
+    Methods
+    -------
+    query_apply()
+        Query the applied settings on the signal generator.
+    turnOn()
+        Turn on the output of the signal generator.
+    turnOff()
+        Turn off the output of the signal generator.
+    setupSine(frequency, amplitude_pp, offset=0, phase=0)
+        Set up the signal generator to produce a sine wave.
+    setupFunc(func, frequency, amplitude_pp, offset=0, phase=0)
+        Set up the signal generator to produce a user-defined function waveform.
+    """
     def __init__(self, gpib_address):
         super().__init__()
         self.query_delay = 0.1
         self.gpib_address = gpib_address
 
     def query_apply(self):
+        """
+        Retrieves the current signal generator configuration by sending the SCPI command "APPLY?".
+
+        Returns
+        -------
+        str
+            The response from the signal generator containing the current configuration.
+        """
         query = self.query_SCPI("APPLY?")
         print(f"Signal Generator setup {query}")
         return query
     
     def turnOn(self):
+        """
+        Turns on the device.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+        """
         self.write_SCPI("OUTPUT ON")
 
     def turnOff(self):
+        """
+        Turns off the device.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+        """
         self.write_SCPI("OUTPUT OFF")
 
     def setupSine(self, frequency, amplitude_pp, offset=0, phase=0):
+        """
+        Set up a sine wave function with the given parameters.
+
+        Parameters
+        ----------
+        frequency : float
+            The frequency of the sine wave.
+        amplitude_pp : float
+            The peak-to-peak amplitude of the sine wave.
+        offset : float, optional
+            The DC offset of the sine wave. Defaults to 0.
+        phase : float, optional
+            The phase offset of the sine wave. Defaults to 0.
+
+        Returns
+        -------
+        str
+            The applied sine wave settings.
+        """
         return self.setupFunc("SIN", frequency, amplitude_pp, offset, phase)
 
     def setupFunc(self, func: str, frequency, amplitude_pp, offset=0, phase=0):
+        """
+        Sets up a function on the instrument.
+
+        Parameters
+        ----------
+        func : str
+            The function to be set up.
+        frequency : 
+            The frequency of the function in Hz.
+        amplitude_pp : 
+            The peak-to-peak amplitude of the function in volts.
+        offset : float, optional
+            The DC offset of the function in volts. Defaults to 0.
+        phase : float, optional
+            The phase offset of the function in degrees. Defaults to 0.
+
+        Returns
+        -------
+        str
+            The applied settings.
+        """
+
         lines = (f"FUNC {func.upper()}",
                     f"FREQ {float(frequency)}",
                     f"VOLT {float(amplitude_pp)}",
--- techconnect/signal_generators/rigol.py
+++ techconnect/signal_generators/rigol.py
@@ -3,11 +3,51 @@ from base._instrument import VISAInstrument, PrologixInstrument
 
 
 class DG1000(VISAInstrument):
+    """
+    Class representing the DG1000 signal generator.
+
+    Parameters
+    ----------
+    None
+
+    Attributes
+    ----------
+    query_delay : int
+        The delay time (in seconds) for SCPI query commands.
+
+    Methods
+    -------
+    query_apply(channel)
+        Queries the signal generator for the current applied settings on the specified channel.
+    turnOn(channel)
+        Turns on the output for the specified channel.
+    turnOff(channel)
+        Turns off the output for the specified channel.
+    setupSine(frequency, amplitude_pp, offset=0, phase=0, channel=1)
+        Sets up the signal generator to produce a sine wave on the specified channel.
+    setupFunc(func, frequency, amplitude_pp, offset=0, phase=0, channel=1)
+        Sets up the signal generator to produce a user-defined function waveform on the specified channel.
+    """
+
     def __init__(self):
         super().__init__()
         self.query_delay = 1
 
     def query_apply(self, channel):
+        """
+        Query applied settings to a specific channel.
+
+        Parameters
+        ----------
+        channel : int
+            The channel number.
+
+        Returns
+        -------
+        str
+            The query response.
+        """
+
         if channel == 1:
             query = self.query_SCPI(f"APPLY?")
         elif channel == 2:
@@ -16,6 +56,19 @@ class DG1000(VISAInstrument):
         return query
     
     def turnOn(self, channel):
+        """
+        Turn on the specified channel.
+
+        Parameters
+        ----------
+        channel : int
+            The channel to turn on. Must be either 1 or 2.
+
+        Returns
+        -------
+        None
+        """
+
         if channel == 1:
             lines = ("OUTPUT ON",)
         if channel == 2:
@@ -24,6 +77,19 @@ class DG1000(VISAInstrument):
         self.write_lines(lines)
 
     def turnOff(self, channel):
+        """
+        Turns off the specified channel.
+
+        Parameters
+        ----------
+        channel : int
+            The channel number to turn off. Valid values are 1 and 2.
+
+        Returns
+        -------
+        None
+            This function does not return anything.
+        """
         if channel == 1:
             lines = ("OUTPUT OFF",)
         if channel == 2:
@@ -32,9 +98,52 @@ class DG1000(VISAInstrument):
         self.write_lines(lines)
 
     def setupSine(self, frequency, amplitude_pp, offset=0, phase=0, channel=1):
+        """
+        Set up the sine wave generator with the specified parameters.
+
+        Parameters
+        ----------
+        frequency : float
+            The frequency of the sine wave in Hz.
+        amplitude_pp : float
+            The peak-to-peak amplitude of the sine wave in volts.
+        offset : float, optional
+            The DC offset of the sine wave in volts. Defaults to 0.
+        phase : float, optional
+            The phase of the sine wave in degrees. Defaults to 0.
+        channel : int, optional
+            The channel number. Defaults to 1.
+
+        Returns
+        -------
+        None
+        """
+
         self.setupFunc("SIN", frequency, amplitude_pp, offset, phase, channel=channel)
 
     def setupFunc(self, func: str, frequency, amplitude_pp, offset=0, phase=0, channel=1):
+        """
+        Set up the function parameters for generating a waveform on a specific channel of the instrument.
+
+        Parameters
+        ----------
+        func : str
+            The type of waveform to generate.
+        frequency : float
+            The frequency of the waveform in Hz.
+        amplitude_pp : float
+            The peak-to-peak amplitude of the waveform in volts.
+        offset : float, optional
+            The DC offset of the waveform in volts. Defaults to 0.
+        phase : float, optional
+            The phase shift of the waveform in degrees. Defaults to 0.
+        channel : int, optional
+            The channel number to generate the waveform on. Defaults to 1.
+
+        Returns
+        -------
+        None
+        """
         if channel == 1:
             lines = (f"FUNC {func.upper()}",
                     f"FREQ {float(frequency)}",
@@ -56,25 +165,139 @@ class DG1000(VISAInstrument):
         
 
 class DG4000(VISAInstrument):
+    """
+    Class representing the DG4000 signal generator.
+
+    Parameters
+    ----------
+    None
+
+    Attributes
+    ----------
+    query_delay : float
+        The delay time (in seconds) for SCPI query commands.
+
+    Methods
+    -------
+    query_apply()
+        Query the applied settings on the signal generator.
+    turnOn(channel)
+        Turn on the output for the specified channel.
+    turnOff(channel)
+        Turn off the output for the specified channel.
+    setupSine(frequency, amplitude_pp, offset=0, phase=0, channel=1)
+        Set up the signal generator to produce a sine wave on the specified channel.
+    setupFunc(func, frequency, amplitude_pp, offset=0, phase=0, channel=1)
+        Set up the signal generator to produce a user-defined function waveform on the specified channel.
+    setupSweep(start_frequency, stop_frequency, amplitude_pp, sweep_time, return_time=0, spacing='LINEAR', trigger_source='INTERNAL', channel=1)
+        Set up the signal generator to produce a sweep waveform on the specified channel.
+    setupAM(frequency, function, depth=100, channel=1)
+        Set up the signal generator to produce an amplitude-modulated waveform on the specified channel.
+    setupRamp(frequency, amplitude_pp, offset=0, phase=0, channel=1, symmetry=50)
+        Set up the signal generator to produce a ramp waveform on the specified channel.
+    """
+
     def __init__(self):
         super().__init__()
         self.query_delay = 0.1
 
     def query_apply(self):
+        """
+        Query applied settings to a specific channel.
+
+        Parameters
+        ----------
+        channel : int
+            The channel number.
+
+        Returns
+        -------
+        str
+            The query response.
+        """
         query = self.query_SCPI(":APPLY?")
         print(f"Signal Generator setup {query}")
         return query
     
     def turnOn(self, channel):
+        """
+        Turn on the specified channel.
+
+        Parameters
+        ----------
+        channel : int
+            The channel to turn on. Must be either 1 or 2.
+
+        Returns
+        -------
+        None
+        """
         self.write_SCPI(f":OUTPUT{channel} ON")
 
     def turnOff(self, channel):
+        """
+        Turns off the specified channel.
+
+        Parameters
+        ----------
+        channel : int
+            The channel number to turn off. Valid values are 1 and 2.
+
+        Returns
+        -------
+        None
+            This function does not return anything.
+        """
         self.write_SCPI(f":OUPUT{channel} OFF")
 
     def setupSine(self, frequency, amplitude_pp, offset=0, phase=0, channel=1):
+        """
+        Set up the sine wave generator with the specified parameters.
+
+        Parameters
+        ----------
+        frequency : float
+            The frequency of the sine wave in Hz.
+        amplitude_pp : float
+            The peak-to-peak amplitude of the sine wave in volts.
+        offset : float, optional
+            The DC offset of the sine wave in volts. Defaults to 0.
+        phase : float, optional
+            The phase of the sine wave in degrees. Defaults to 0.
+        channel : int, optional
+            The channel number. Defaults to 1.
+
+        Returns
+        -------
+        str
+            The applied sine wave settings.
+        """
         return self.setupFunc("SIN", frequency, amplitude_pp, offset, phase, channel)
 
     def setupFunc(self, func: str, frequency, amplitude_pp, offset, phase, channel):
+        """
+        Set up the function parameters for generating a waveform on a specific channel of the instrument.
+
+        Parameters
+        ----------
+        func : str
+            The type of waveform to generate.
+        frequency : float
+            The frequency of the waveform in Hz.
+        amplitude_pp : float
+            The peak-to-peak amplitude of the waveform in volts.
+        offset : float, optional
+            The DC offset of the waveform in volts. Defaults to 0.
+        phase : float, optional
+            The phase shift of the waveform in degrees. Defaults to 0.
+        channel : int, optional
+            The channel number to generate the waveform on. Defaults to 1.
+
+        Returns
+        -------
+        str
+            The applied settings.
+        """
         lines = (f":SOURCE{channel}:FUNC {func.upper()}",
                     f":SOURCE{channel}:FREQ:FIXED: {float(frequency)}",
                     f":SOURCE{channel}:VOLT {float(amplitude_pp)}",
@@ -87,6 +310,34 @@ class DG4000(VISAInstrument):
     
     def setupSweep(self, start_frequency, stop_frequency, amplitude_pp, sweep_time, 
                    return_time=0, spacing='LINEAR', trigger_source='INTERNAL', channel=1):
+        """
+        Sets up a sweep on a specified channel of the instrument.
+
+        Parameters
+        ----------
+        start_frequency : float
+            The start frequency of the sweep.
+        stop_frequency : float
+            The stop frequency of the sweep.
+        amplitude_pp : float
+            The peak-to-peak amplitude of the sweep.
+        sweep_time : float
+            The duration of the sweep.
+        return_time : float, optional
+            The return time of the sweep. Defaults to 0.
+        spacing : str, optional
+            The spacing of the sweep. Defaults to 'LINEAR'.
+        trigger_source : str, optional
+            The trigger source of the sweep. Defaults to 'INTERNAL'.
+        channel : int, optional
+            The channel to set up the sweep on. Defaults to 1.
+
+        Returns
+        -------
+        str
+            The applied sweep settings.
+        """
+
         lines = (f":SOURCE{channel}:FUNC SWEEP",
                     f":SOURCE{channel}:FREQ:START {float(start_frequency)}",
                     f":SOURCE{channel}:FREQ:STOP {float(stop_frequency)}",
@@ -102,6 +353,26 @@ class DG4000(VISAInstrument):
         return self.query_apply()
     
     def setupAM(self, frequency, function, depth=100, channel=1):
+        """
+        Sets up the amplitude modulation (AM) for a specific frequency, function, depth, and channel.
+
+        Parameters
+        ----------
+        frequency : float
+            The frequency of the AM signal.
+        function : str
+            The function used for the AM modulation.
+        depth : float, optional
+            The depth of the AM modulation. Defaults to 100%.
+        channel : int, optional
+            The channel number. Defaults to 1.
+
+        Returns
+        -------
+        str
+           The applied AM settings.
+        """
+
         lines = (f":SOURCE{channel}:MOD:TYPE AM",
                     f":SOURCE{channel}:MOD:AM:INTERNAL:FUNC {function.upper()}",
                     f":SOURCE{channel}:MOD:AM:INTERNAL:FREQ: {float(frequency)}",
@@ -113,5 +384,28 @@ class DG4000(VISAInstrument):
         return self.query_apply()
     
     def setupRamp(self, frequency, amplitude_pp, offset=0, phase=0, channel=1, symmetry=50):
+        """
+        Set up a ramp waveform on the specified channel.
+
+        Parameters
+        ----------
+        frequency : float
+            The frequency of the ramp waveform in Hz.
+        amplitude_pp : float
+            The peak-to-peak amplitude of the ramp waveform in volts.
+        offset : float, optional
+            The DC offset of the ramp waveform in volts. Defaults to 0.
+        phase : float, optional
+            The phase offset of the ramp waveform in degrees. Defaults to 0.
+        channel : int, optional
+            The channel number to set up the ramp waveform on. Defaults to 1.
+        symmetry : int, optional
+            The symmetry percentage of the ramp waveform. Defaults to 50.
+
+        Returns
+        -------
+        str
+            The applied ramp settings.
+        """
         self.write_SCPI(f"SOURCE{channel}:FUNC:RAMP:SYMMETRY:{symmetry}")
         return self.setupFunc("RAMP", frequency, amplitude_pp, offset, phase, channel)
